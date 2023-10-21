f2d964418414ceb60300c4b46f3066b2a27b6bd5
HLF <106381172+hoolyfook@users.noreply.github.com>
2023-10-21 22:24:11 +0700

    task 3

---
+++ ass3/task3/index.js
@@ -0,0 +1,16 @@
+$("#back-to-top").fadeIn(),
+$("#back-to-top").click((function() {
+    return $("body,html").animate({
+        scrollTop: 0
+    }, 200),
+    !1
+}
+));
+
+$("#back-to-bottom").fadeIn(),
+$("#back-to-bottom").click((function() {
+    return $("body, html").animate({ 
+        scrollTop: $(document).height() 
+    }, 200)
+}
+));
