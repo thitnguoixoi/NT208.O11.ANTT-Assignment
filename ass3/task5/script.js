e60019c0b281b2355ef1bfefd3c6a12806a4d9a1
thitnguoixoi <vanlong03062003@gmail.com>
2023-10-23 23:21:32 +0700

    fixtask5

--- ass3/task5/script.js
+++ ass3/task5/script.js
@@ -1,50 +1,51 @@
 $("#validation-form").validate({
-    rules:{
-        name:{
+    rules: {
+        name: {
             minlength: 4
         },
-        phone:{
-            number:true,
+        phone: {
+            number: true,
             minlength: 10,
             maxlength: 10
         },
-        email:{
-            email:true
+        email: {
+            email: true,
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
         },
-        password:{
+        password: {
             minlength: 5
         },
-        repassword:{
-            minlength:5,
-            equalTo:'#password'
+        repassword: {
+            minlength: 5,
+            equalTo: '#password'
         }
     },
-    messages:{
-        name:{
+    messages: {
+        name: {
             required: "Vui lòng nhập tên đăng nhập",
             minlength: "Tên đăng nhập phải lớn hơn 4 kí tự"
         },
-        phone:{
+        phone: {
             required: "Vui lòng nhập số điện thoại",
             number: "Vui lòng nhập số điện thoại hợp lệ",
             minlength: "Số điện thoại bạn vừa nhập không tồn tại",
             maxlength: "Số điện thoại bạn vừa nhập không tồn tại"
         },
-        email:{
+        email: {
             required: "Vui lòng nhập Email",
             email: "Vui lòng nhập Email theo đúng định dạng"
         },
-        password:{
+        password: {
             required: "Vui lòng nhập mật khẩu",
             minlength: "Mật khẩu cần có tối thiểu 5 kí tự"
         },
-        repassword:{
+        repassword: {
             required: "Vui lòng nhập lại mật khẩu",
             equalTo: "Mật khẩu không trùng"
         }
     },
 
-    submitHandler: function(form) {
-      form.submit();
+    submitHandler: function (form) {
+        form.submit();
     }
 });
